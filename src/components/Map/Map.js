c4cbfcd6d647c46a6fd2a1a8fd62fd4563127985
KaziBubka <69438390+KaziBubka@users.noreply.github.com>
2021-07-18 23:22:26 -0400

    changed dependancy array in useEffect to include countryCoordinates

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -23,38 +23,28 @@ const Map = ({ country, countryCoordinates }) => {
         if (country) {
             try {
                 setCenter({lat: countryCoordinates.latitude, lng: countryCoordinates.longitude})
-                setZoom(8)
+                setZoom(6)
             } catch (error) {
                 console.log(error)
             }
         }
-    }, [country])
+    }, [countryCoordinates])
 
     //set markers based on selected country lat lng
 
-    // const markers = countryCoordinates.map(ev => {
-    //     return <LocationMarker lat={ev.latitude} lng={ev.longitude} />
-    // })
 
     return (
         <div className={styles.map}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: 'GOOGLE-MAPS-API-KEY'}}
+                center={ center }
                 defaultCenter={ center }
-                defaultZoom={ zoom }
+                zoom={ zoom }
             >
                 <LocationMarker lat={center.lat} lng={center.lng} />
             </GoogleMapReact>
         </div>
     )
 }
-
-// Map.defaultProps = {
-//     center: {
-//         lat: 45.4215,
-//         lng: -75.6971
-//     },
-//     zoom: 1
-// }
 
 export default Map
